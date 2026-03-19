a2e264ce11569e82fae22074200b4a6b4c026040
rizvyshaik1222 <your-email@example.com>
2026-03-19 15:25:46 +0530

    config: update base URL for annviproject repo

--- Desktop/HR Module Project/frontend/vite.config.js
+++ Desktop/HR Module Project/frontend/vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/HR-Mangement-System/',
+  base: '/annviproject/',
   plugins: [react()],
   server: {
     port: 5173
